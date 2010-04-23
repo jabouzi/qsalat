c360038749354338079a36d1d6a027d1e387f967
Skander Jabouzi <jabouzi@gmail.com>
2010-04-23 23:32:35 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/qsalat/trunk@5 ea0bdeb7-57c4-4e52-a4b7-181e3d1c9d5a

---
+++ src/alarm.cpp
@@ -0,0 +1,95 @@
+#include "alarm.h"
+
+Alarm::Alarm(QObject* parent) :
+    QObject(parent)
+{
+
+}
+
+void Alarm::init()
+{
+    QDateTime today = QDateTime::currentDateTime();
+    year = today.date().year();
+    month = today.date().month();
+    day = today.date().day();
+    hours = 0;
+    minutes = 0;
+    seconds = 0;
+    timeToAlarm = 0;
+    timer = new QTimer(this);
+    connect(timer, SIGNAL(timeout()), this, SLOT(finishAalarm()));
+}
+
+void Alarm::setAlarm()
+{
+    QDateTime now = QDateTime::currentDateTime();
+    QDateTime alarmTime(QDate(year, month, day), QTime(hours, minutes, seconds));
+    timeLeft = now.secsTo(alarmTime);
+    if (timeLeft > 0)
+    {
+        qDebug("There are %d seconds to alarm", now.secsTo(alarmTime));
+        timeToAlarm = timeLeft*1000;
+        startAlarm();
+    }
+    else
+    {
+        qDebug("There are %d seconds passed", abs(now.secsTo(alarmTime)));
+        timer->stop(); 
+    }
+}
+
+void Alarm::startAlarm()
+{
+    timer->start(timeToAlarm);
+}
+
+void Alarm::stopAlarm()
+{
+    timer->stop();
+}
+
+void Alarm::setYear(int year_)
+{
+    year = year_;
+}
+
+void Alarm::setMonth(int month_)
+{
+    month = month_;
+}
+
+void Alarm::setDay(int day_)
+{
+    day = day_;
+}
+
+void Alarm::setHours(int hours_)
+{
+    hours = hours_;
+}
+
+void Alarm::setMinutes(int minutes_)
+{
+    minutes = minutes_;
+}
+
+void Alarm::setSeconds(int seconds_)
+{
+    seconds = seconds_;
+}
+
+void Alarm::finishAalarm()
+{
+    timer->stop(); 
+    emit(itsTime());   
+}
+
+void Alarm::printMassage()
+{
+    qDebug("Alarm finished");
+}
+
+int Alarm::getTimeLeft()
+{
+    return timeLeft;
+}
---
+++ src/alarm.h
@@ -0,0 +1,47 @@
+#ifndef ALARM_H
+#define ALARM_H
+
+#include <QObject>
+#include <QTimer> 
+#include <QDateTime>
+#include <QCoreApplication>
+#include <stdlib.h>
+
+class Alarm : public QObject
+{  
+  Q_OBJECT     
+    public:
+        Alarm(QObject* = 0); 
+        void startAlarm();
+        void stopAlarm();   
+        void setYear(int);
+        void setMonth(int);
+        void setDay(int); 
+        void setHours(int);
+        void setMinutes(int);
+        void setSeconds(int);
+        void setAlarm();
+        void init();
+        int getTimeLeft();
+    
+    private:    
+        int year;
+        int month;
+        int day;
+        int hours;
+        int minutes;
+        int seconds;
+        int timeToAlarm;
+        int timeLeft;
+        QTimer *timer;       
+        
+    private slots:
+        void finishAalarm();
+        void printMassage();
+        
+    signals:
+        void itsTime();
+    
+};
+
+#endif
