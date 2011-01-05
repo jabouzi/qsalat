717c29a1cda93add36f6969f6978426863ec8ae8
Skander Jabouzi <jabouzi@gmail.com>
2011-01-05 03:15:31 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/qsalat/trunk@87 ea0bdeb7-57c4-4e52-a4b7-181e3d1c9d5a

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,9 @@ int main(int argc, char ** argv)
     app.setApplicationName( "Qsalat" );
     QString locale = QLocale::system().name();
     QTranslator translator;
-    translator.load(QString(locale+"/qsalat_") + locale);
+    //translator.load(QString("qsalat_fr") + locale);
+    if (locale.contains("fr", Qt::CaseInsensitive)) translator.load(QString("fr/qsalat_fr"));
+    else if (locale.contains("en", Qt::CaseInsensitive)) translator.load(QString("en/qsalat_en"));
     app.installTranslator(&translator);
     Qsalat win;
     win.show(); 
